94633e122b3b20fbd66edc35c61f11ebb151f23e
akashhossan07 <37851065+akashossan07@users.noreply.github.com>
2020-03-29 18:52:05 -0700

    Youtube video player working

---
+++ app/src/main/java/com/example/videostreamapp/YoutubeConfig.java
@@ -0,0 +1,10 @@
+package com.example.videostreamapp;
+
+public class YoutubeConfig {
+    public YoutubeConfig(){
+    }
+    private static final String API_KEY = "AIzaSyA9MEH-ROXsSwq-_I7M5oGrBpk3Qzu_kJk";
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
